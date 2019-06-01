3001cb36d47f07a70952b0cf8624cdc70a754b7c
James Dube <jamesdylandube@gmail.com>
2019-06-01 23:38:59 +0200

    initial commit

---
+++ matcher-service/src/test/java/com/jamesdube/matcherservice/MatcherServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jamesdube.matcherservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatcherServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
